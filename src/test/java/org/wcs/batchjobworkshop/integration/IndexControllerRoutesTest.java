b7fad9b2bf534731066024f2745eddc82a720003
Nicolas Rousset <nrousset@gmail.com>
2022-01-06 11:31:24 +0100

    Adding integration test of index '/'

---
+++ src/test/java/org/wcs/batchjobworkshop/integration/IndexControllerRoutesTest.java
@@ -0,0 +1,36 @@
+package org.wcs.batchjobworkshop.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class IndexControllerRoutesTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void index() throws Exception {
+        MockHttpServletResponse mockHttpServletResponse = mvc.perform(get("/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(
+                "{\"message\":\"Welcome to batch job workshop\"}",
+                mockHttpServletResponse.getContentAsString());
+
+    }
+}
